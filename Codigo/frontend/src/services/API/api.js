8e5e5f59d83c68606ce73ad1a51ab1e4dd298a82
Marcos <filips8@hotmail.com>
2023-04-25 23:34:04 -0300

    front

--- Codigo/frontend/src/services/API/api.js
+++ Codigo/frontend/src/services/API/api.js
@@ -1,16 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:9999"
+  baseURL: "http://localhost:9999"
 });
 
-api.interceptors.request.use(async config => {
-const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkBhZG1pbi5jb20iLCJleHAiOjE2ODI0ODQ1NDEsImlhdCI6MTY4MjQ2NjU0MX0.RthoHRyZob6TEV_LG4GY1qlsBBY06f7Ktxw7_AvnCWwuT0XJlx8YjZaUCnvr_cEug-sMymqHct_l-ozZbXNkdQ";
-  if (token) {
-    config.headers.authorization = `Bearer ` + token;
-    console.log("API")
-  }
-  return config;
-});
+const accessToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkBhZG1pbi5jb20iLCJleHAiOjE2ODI0ODQ1NDEsImlhdCI6MTY4MjQ2NjU0MX0.RthoHRyZob6TEV_LG4GY1qlsBBY06f7Ktxw7_AvnCWwuT0XJlx8YjZaUCnvr_cEug-sMymqHct_l-ozZbXNkdQ";
+
+api.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
 
 export default api;
